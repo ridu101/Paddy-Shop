930bf426b847856048dbef2216599433969662a0
ridu101 <ridu116540@gmail.com>
2025-10-07 19:34:17 +0600

    all pets loaded

--- script.js
+++ script.js
@@ -29,9 +29,10 @@ loadCategories()
 
 const loadAllPets= async () =>{
     const res= await fetch("https://openapi.programming-hero.com/api/peddy/pets")
-    const data = await res.json();
+    const data = await res.json()
+    displayAllPets(data.pets)
 
-    console.log(data)
+    
 }
 
 const displayAllPets=(pets)=>{
@@ -41,10 +42,26 @@ const displayAllPets=(pets)=>{
     for (const pet of pets){
         const petCard= document.createElement("div");
         petCard.innerHTML=`
-        
-        
-        
+        <div class="p-5 shadow-xl rounded-lg w-[312px] h-full">
+                    <div>
+                        <img class="rounded-lg" src="${pet.image}" alt="">
+                        <h1 class="font-bold text-2xl">${pet.category}</h1>
+                        <div class="flex items-center space-x-3"><i class="fa-solid fa-table-cells-large"></i> <p>Breed:${pet.breed}</p></div>
+                        <div class="flex items-center space-x-3 mt-5"><i class="fa-solid fa-calendar"></i><p>Birth: ${pet.date_of_birth}</p></i></div>
+                        <div class="flex items-center space-x-3"><i class="fa-solid fa-mars-stroke-up"></i><p>Gender:${pet.gender}</p></i></div>
+                        <div class="flex items-center space-x-3"><i class="fa-solid fa-dollar-sign"></i><p>Price: ${pet.price}</p></i></div>
+                        
+                    </div>
+                   
+                    <div class=" space-x-2 mt-5 flex justify-center">
+                        
+                        <button class="btn"><i class="fa-solid fa-thumbs-up"></i></button>
+                        <button class="btn"> Adopt</button>
+                        <button class="btn"> Details</button>
+                    </div>
+                 </div>
         `
+        petsContainer.appendChild(petCard)
     }
 }
 loadAllPets()
